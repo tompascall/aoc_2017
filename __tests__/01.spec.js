1094df926027b90d8208e4f099162c15b2532508
Gulacsy-Toth <tompascall@gmail.com>
2017-12-04 20:59:30 +0100

    solve day 01

--- __tests__/01.spec.js
+++ __tests__/01.spec.js
@@ -1,7 +1,17 @@
-const { add } = require('../src/01.js');
+const day = require('../days/01.js');
 
-describe('test framework', () => {
-    it('adds two numbers', () => {
-        expect(add(1, 3)).toEqual(4);
+describe('day 01', () => {
+    it('gives back the sum of digits if digits are the same', () => {
+        expect(day('1111')).toEqual(4);
+        expect(day('11111')).toEqual(5);
+    });
+
+    it('gives back zero if all digits are different', () => {
+        expect(day('1234')).toEqual(0);
+    });
+
+    it('gives back a digit if it matches to the next one (nput is circular)', () => {
+        expect(day('12234')).toEqual(2);
+        expect(day('1231')).toEqual(1);
     });
 });
--- days/01.js
+++ days/01.js
@@ -1,5 +1,15 @@
-exports.add = (x, y) => x + y;
+const getDigits = (input) => input.split('').map(x => +x);
+
+const getNextDigit = (digits, currentIndex) => {
+    return (currentIndex < digits.length - 1) ?
+        digits[currentIndex + 1] : digits[0];
+};
 
 module.exports = (input) => {
-    console.log(input);
+    const digits = getDigits(input);
+    return digits.reduce((acc, digit, index) => {
+        const nextDigit = getNextDigit(digits, index);
+        if (digit === nextDigit) acc += digit;
+        return acc;
+    }, 0)
 };
